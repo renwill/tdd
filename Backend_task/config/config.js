9656822044ad821e8f47dfa97839f54e35b0f0c3
Lynn Mao <lynn.mao@oocl.com>
2016-08-01 10:26:44 +0800

    Remove obsolete confg

--- Backend_task/config/config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by MAOLY on 1/29/2016.
- */
-var env = 'env';
-if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'){
-    env = process.env.NODE_ENV;
-}
-module.exports = require('./env/' + env + '.js');
